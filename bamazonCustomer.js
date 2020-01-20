fbb0a345a60402efe0f905a9913dbadc8ca4a327
Justin Cheng <ijustin17@gmail.com>
2020-01-20 00:03:03 -0600

    finished

---
+++ bamazonCustomer.js
@@ -0,0 +1,89 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "1Fungi99",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+var SKUArray = [];
+console.log("Selecting all products...\n");
+connection.query("SELECT * FROM products", function(err, res) {
+  if (err) throw err;
+  // Log all results of the SELECT statement
+  for (var i = 0; i < res.length; i++) {
+    console.log("SKU: " + res[i].item_ID);
+    console.log("Product: " + res[i].product_name);
+    console.log("Department: " + res[i].department_name);
+    console.log("Price: " + res[i].price);
+    console.log("Stock: " + res[i].stock_quantity);
+    console.log(" ");
+
+    SKUArray.push(res[i].item_ID);
+  }
+});
+var inquirer = require("inquirer");
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "sku",
+      message: "What SKU whould you like to buy?",
+      validate: function(value) {
+        var valid = SKUArray.includes(value);
+        return valid || "Please Re-Check the SKU";
+      },
+      filter: Number
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "How many do you need?",
+      validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || "Please enter a number";
+      },
+      filter: Number
+    }
+  ])
+  .then(answers => {
+    connection.query(
+      "SELECT * FROM products WHERE item_ID = " + answers.sku,
+      function(err, res) {
+        if (err) throw err;
+        else if (answers.quantity > res.stock_quantity) {
+          console.log(
+            "We're sorry, but it seems that we are not able to fulfill your order at this time..."
+          );
+        } else {
+          //   console.log("Server stock: " + res[0].stock_quantity);
+          //   console.log("Requested stock adjustment: " + answers.quantity);
+          var updatedStock = res[0].stock_quantity - answers.quantity;
+          //   console.log(updatedStock);
+          connection.query(
+            "UPDATE products SET stock_quantity = " +
+              updatedStock +
+              " WHERE item_ID = " +
+              answers.sku,
+            function(err, res) {
+              if (err) throw err;
+              console.log(res);
+              connection.end();
+            }
+          );
+        }
+      }
+    );
+  });
